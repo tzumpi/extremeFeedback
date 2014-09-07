1eb20767af0ffde97647f02228b2f429ebb6a660
Ioana Dima <ioana.maties@gmail.com>
2014-09-07 14:00:57 +0300

    second commit to fail the jenkins job

--- src/TestMainClass.java
+++ src/TestMainClass.java
@@ -1,11 +1,19 @@
 
 public class TestMainClass {
 
+	private static void call() throws Exception{
+		throw new Exception();
+	}
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int number = "mama";
+		try {
+		    call();
+		} catch (Exception e) {
+		    System.out.println("An exception was caught at the top level:" + e);
+		    System.exit(-1);
+		}
 
 	}
 
